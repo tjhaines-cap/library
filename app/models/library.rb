e61dd214b7fb6d95a32ffb350cf31e4c6635afc2
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-12 14:50:23 -0600

    Feat: add validatations to library model

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,5 +1,5 @@
 class Library < ApplicationRecord
-    # has_many :books
-    # validates_presence_of :name, :branch_num, :city
-    # validates :open, inclusion: [true, false]
+    has_many :books
+    validates_presence_of :name, :branch_num, :city
+    validates :open, inclusion: [true, false]
 end
