1a91c9f0e94934520be87097fb18141f25e9bd3c
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-12 14:49:45 -0600

    Test: library validations

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Library, type: :model do
     
     describe 'validations' do
-
+        it { should validate_presence_of :name}
+        it { should validate_presence_of :branch_num}
+        it { should validate_presence_of :city}
+        it { should allow_value(true).for(:open) }
+        it { should allow_value(false).for(:open) }
     end
 
     describe 'relationships' do
