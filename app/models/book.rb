0b99804c4686c3c7642b48a5a71f2479806d86b5
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-12 20:52:39 -0600

    Feat: add belongs to relationship between book and library

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-    # belongs_to :library
+    belongs_to :library
     validates_presence_of :title, :author, :copyright
     validates :available, inclusion: [true, false]
 end
