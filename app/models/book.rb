44081437e433e10adf37a1d165212aecfbf523e8
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-12 20:49:29 -0600

    Feat: add validations for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
     # belongs_to :library
+    validates_presence_of :title, :author, :copyright
+    validates :available, inclusion: [true, false]
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,9 +6,8 @@ RSpec.describe Book, type: :model do
         it { should validate_presence_of :title }
         it { should validate_presence_of :author }
         it { should validate_presence_of :copyright }
-        it { should validate_presence_of :available }
-        # it { should allow_value(true).for(:available) }
-        # it { should allow_value(false).for(:available) }
+        it { should allow_value(true).for(:available) }
+        it { should allow_value(false).for(:available) }
     end
 
     describe 'relationships' do
