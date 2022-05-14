3ca8877f9c2007ae2343b14b302cd056a7b30745
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-14 08:09:10 -0600

    Test: child index link on library index page

--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe 'library index page' do
         expect(page).to have_content("#{library.name} created: #{library.created_at}")
         expect(page).to have_content("#{library2.name} created: #{library2.created_at}")
     end
+
+    it 'has link to child index page' do
+        visit "/libraries"
+
+        click_on "All books"
+
+        expect(current_path).to eq("/books")
+    end
 end
