d06843bccaca64361fdb2a8e3804a736765927cb
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-15 09:55:08 -0600

    Feat: link to new library works

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -6,4 +6,8 @@ class LibrariesController < ApplicationController
     def show
         @library = Library.find(params[:id])
     end
+
+    def new
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/libraries', to:'libraries#index'
+  get '/libraries/new', to: 'libraries#new'
   get '/libraries/:id', to: 'libraries#show'
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
