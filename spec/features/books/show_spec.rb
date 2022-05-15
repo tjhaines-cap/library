8f984269f544d74a3faf1c7e452ea0e35e3db44c
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-15 08:48:04 -0600

    Feat: test and add link on each remaining page to library index

--- spec/features/books/show_spec.rb
+++ spec/features/books/show_spec.rb
@@ -52,4 +52,11 @@ RSpec.describe 'books show page' do
         expect(current_path).to eq("/books")
     end
 
+    it 'has link to libraries index' do
+        visit "/books/#{@book1.id}"
+        
+        click_on "View all libraries"
+        
+        expect(current_path).to eq("/libraries")
+    end
 end
