aa4e94589480a357588557cc7c50abdbc655990c
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-15 09:26:14 -0600

    Feat: add link on library show page to its book list

--- spec/features/libraries/show_spec.rb
+++ spec/features/libraries/show_spec.rb
@@ -70,10 +70,11 @@ RSpec.describe 'library show page' do
 
     it 'has link to the books show page of this library' do
         sheridan = Library.create!(name: "Sheridan", branch_num: 3, city: "Denver", open: false)
-
+        sheridan.books.create!(title: "Pride and Prejudice", author: "Jane Austen", copyright: 1813, available: true)
+        
         visit "/libraries/#{sheridan.id}"
         
-        click_on "View #{sheridan.name} library books list"
+        click_on "View #{sheridan.name} library book list"
 
         expect(current_path).to eq("/libraries/#{sheridan.id}/books")
     end
