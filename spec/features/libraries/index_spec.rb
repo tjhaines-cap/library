9872d9cbbb6e8ec5a42bce34f4d5c2f6ab1a1a0d
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-16 15:47:05 -0600

    Test: order of libraries on library index page

--- spec/features/libraries/index_spec.rb
+++ spec/features/libraries/index_spec.rb
@@ -18,5 +18,29 @@ RSpec.describe 'library index page', type: :feature do
         expect(current_path).to eq("/books")
     end
 
+    it 'displays the libraries in the order they were created, most recent first' do
+        library = Library.create!(name: "Koelbel", branch_num: 1, city: "Centennial", open: true)
+        library2 = Library.create!(name: "Sheridan", branch_num: 2, city: "Denver", open: false)
+        library3 = Library.create!(name: "Smoky Hill", branch_num: 5, city: "Centennial", open: true)
+        
+        visit "/libraries"
+        
+        within('#library-0') do
+            expect(page).to have_content("Smoky Hill")
+            expect(page).to_not have_content("Sheridan")
+            expect(page).to_not have_content("Koelbel")
+        end
+        within('#library-1') do
+            expect(page).to have_content("Sheridan")
+            expect(page).to_not have_content("Koelbel")
+            expect(page).to_not have_content("Smoky Hill")
+        end
+        within('#library-2') do
+            expect(page).to have_content("Koelbel")
+            expect(page).to_not have_content("Sheridan")
+            expect(page).to_not have_content("Smoky Hill")
+        end
+    end
+
     
 end
