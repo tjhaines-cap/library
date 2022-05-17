d8ea3f1dcc35590823bdaa3ec38f0fa9da1742e8
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-16 20:57:53 -0600

    Feat: add available method to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
     belongs_to :library
     validates_presence_of :title, :author, :copyright
     validates :available, inclusion: [true, false]
+
+    def self.available
+        Book.where(available: true)
+    end
 end
