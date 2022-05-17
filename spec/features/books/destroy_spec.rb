688b412909aff45b18b381e100ecb355a2d039ae
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-17 17:46:57 -0600

    Test: delete book

---
+++ spec/features/books/destroy_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'destroy book', type: :feature do
+
+    it 'can delete a book from a link on the show page' do
+        koelbel = Library.create!(name: "Koelbel", branch_num: 1, city: "Centennial", open: true)
+        book1 = koelbel.books.create!(title: "Leaves of Grass", author: "Walt Whitman", copyright: 1855, available: true)
+        book2 = koelbel.books.create!(title: "The Sea-Wolf", author: "Jack London", copyright: 1904, available: true)
+        sheridan = Library.create!(name: "Sheridan", branch_num: 3, city: "Denver", open: false)
+        book3 = sheridan.books.create!(title: "Of Mice and Men", author: "John Steinbeck", copyright: 1937, available: false)
+        book4 = sheridan.books.create!(title: "Pride and Prejudice", author: "Jane Austen", copyright: 1813, available: true)
+
+        visit "/books/#{book2.id}"
+        click_link("Delete #{book2.title}")
+
+        expect(current_path).to eq("/books")
+        expect(page).to_not have_content("The Sea-Wolf")
+        expect(page).to have_content("Leaves of Grass")
+    end
+
+end
