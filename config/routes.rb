283d1fc57e5a3d03f6cd18f4190fe4b42c547b0a
tjhaines-cap <tjhaines@eckerd.edu>
2022-05-22 11:47:45 -0600

    Ref: organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,21 @@ Rails.application.routes.draw do
   get '/libraries', to:'libraries#index'
   get '/libraries/new', to: 'libraries#new'
   get '/libraries/:id', to: 'libraries#show'
-  get '/books', to: 'books#index'
-  get '/books/:id', to: 'books#show'
-  get '/libraries/:library_id/books', to: 'library_books#index'
   post '/libraries', to: 'libraries#create'
   get '/libraries/:library_id/edit', to: 'libraries#edit'
   patch '/libraries/:library_id', to: 'libraries#update'
-  get '/libraries/:library_id/books/new', to: 'library_books#new'
-  post '/libraries/:library_id/books', to: 'library_books#create'
-  get '/books/:book_id/edit', to: 'books#edit'
-  post '/books/:book_id', to: 'books#update'
-  get '/libraries/:library_id/books/sort', to: 'library_books#sort'
   delete '/libraries/:library_id', to: 'libraries#destroy'
   delete '/books/:book_id', to: 'books#destroy'
+ 
+
+  get '/books', to: 'books#index'
+  get '/books/:id', to: 'books#show'
+  get '/books/:book_id/edit', to: 'books#edit'
+  post '/books/:book_id', to: 'books#update'
+  
+
+  get '/libraries/:library_id/books', to: 'library_books#index'
+  get '/libraries/:library_id/books/new', to: 'library_books#new'
+  post '/libraries/:library_id/books', to: 'library_books#create'
+  get '/libraries/:library_id/books/sort', to: 'library_books#sort'
 end
